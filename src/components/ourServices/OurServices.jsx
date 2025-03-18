d0157132ea2ea8a51651f3e95b236d0e52ccad50
Nando Alvarado <nandoalvarado022@gmail.com>
2025-03-17 23:35:39 -0500

    Estilos a servicios ofrecidos en onboarding

--- src/components/ourServices/OurServices.jsx
+++ src/components/ourServices/OurServices.jsx
@@ -5,25 +5,27 @@ const OurServices = () => {
         <div className={styles.OurServicesComponent}>
             <li className='Card'>
                 <span className={styles.icon}>
-                    <img src="https://150800256.v2.pressablecdn.com/wp-content/uploads/2022/01/logo.png" alt="" />
+                    <img src="/images/logos/celu.svg" alt="" />
                 </span>
-                <b>Integraciones</b>
-                &nbsp;con pasarelas de pagos, recibe pagos en linea, tarjetas de credito/debito, efecty, etc.
-                <br />ADDI, Sistecredito,
+                <div>
+                    <b>Integraciones</b>
+                    &nbsp;con pasarelas de pagos, recibe pagos en linea, tarjetas de credito/debito, efecty, etc.
+                    ADDI, Sistecredito.
+                </div>
             </li>
             {/* <li className='Card'>
                 <b>Pikcredit</b>
             </li> */}
             <li className='Card'>
                 <span className={styles.icon}>
-                    <img src="https://statics.forbes.com.ec/2023/10/653963100ba8c.jpg" alt="" />
+                    <img src="/images/logos/desarrollo.svg" alt="" />
                 </span>
                 <b>Desarollo a la medida</b>
                 &nbsp; facturacion electronica, inventario, contabilidad, etc.
             </li>
             <li className='Card'>
                 <span className={styles.icon}>
-                    <img src="https://img.freepik.com/vector-gratis/chatbot-mensaje-chat-vectorart_78370-4104.jpg" alt="" />
+                    <img src="/images/logos/chatbot.svg" alt="" />
                 </span>
                 <b>Chatbot</b>
                 &nbsp;para mejorar la efiencia en la atencion al cliente. Enfocate en tu negocio y nosotros nos encargamos de la tecnologia.
